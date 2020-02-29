11e34af19f67fe89163b26fd0a73203b2b2a5f8f
Craig-Benson <craigbenson@outlook.ie>
2020-02-29 14:15:00 +0000

    changed code, so the neighboursis actually working

--- src/com/company/Board.java
+++ src/com/company/Board.java
@@ -6,9 +6,10 @@ import java.util.*;
 public class Board {
 
     // create a board from an n-by-n array of tiles,
-    // where tiles[row][col] = tile at (row, col)
+// where tiles[row][col] = tile at (row, col)
     private int[][] tiles;
     private int[][] goal;
+    private int size;
 
 
     public Board(int[][] tiles) {
@@ -20,10 +21,15 @@ public class Board {
 
         this.tiles = tiles;
 
+        size = dimension();
     }
+
 
     // unit testing (not graded)
     public static void main(String[] args) {
+
+
+
     }
 
     // string representation of this board
@@ -33,20 +39,20 @@ public class Board {
 
     // board dimension n
     public int dimension() {
-        return tiles[0].length * tiles.length;
+        return tiles.length;
     }
 
     // number of tiles out of place
     public int hamming() {
 
 
-
         int count = 0;
 
-        for (int i = 0; i <= tiles.length - 1; i++) {
-            for (int j = 0; j <= tiles.length - 1; j++) {
-                if (tiles[i][j] == goal[i][j]) {
+        for (int row = 0; row <= size; row++) {
+            for (int col = 0; col <= size; col++) {
+                if (tiles[row][col] != goal[row][col]) {
                     count++;
+                    manhattan();
                 }
             }
         }
@@ -57,29 +63,140 @@ public class Board {
     // sum of Manhattan distances between tiles and goal
     public int manhattan() {
 
+// {0, 1, 3},
+// {4, 2, 5},
+// {7, 8, 6},
 
-        return 0;
+        int sum = 0;
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+
+                if ((tiles[i][j] != 0) && (tiles[i][j] != goal[i][j])) {
+                    int goalI = (tiles[i][j] - 1 ) / size;
+                    int goalJ = (tiles[i][j] - 1) % size;
+
+// sum vertical and horizontal difference in distance
+                    sum += Math.abs(i - goalI) + Math.abs(j - goalJ);
+                }
+            }
+        }
+
+        return sum;
     }
 
     // is this board the goal board?
     public boolean isGoal() {
-        return false;
+        return manhattan() == 0;
     }
 
     // does this board equal y?
     public boolean equals(Object y) {
-        return false;
+
+        if (y == this) {
+            return true;
+        }
+        if (y == null) {
+            return false;
+        }
+        if (y.getClass() != this.getClass()) return false;
+
+        Board that = (Board) y;
+        return (Arrays.equals(that.tiles,this.tiles));
     }
 
     // all neighboring boards
     public Iterable<Board> neighbors() {
-        return null;
+
+
+
+        return findZero();
+    }
+
+    private Iterable<Board> findZero() {
+
+        int row = 0;
+        int col = 0;
+
+        for (row = 0; row < size; row++) {
+            for (col = 0; col < size; col++) {
+
+                if (tiles[row][col] == 0) {
+
+                    return addNeighbours(row,col);
+
+
+
+                }
+
+            }
+        }
+return null;
+    }
+
+    private Iterable<Board> addNeighbours(int row, int col) {
+        ArrayList<Board> neighbours = new ArrayList<>();
+
+        if(row > 0){
+//top n
+            neighbours.add(swap(row,col, row-1, col));
+
+        }
+         if(col > 0){
+//right n
+            neighbours.add(swap(row,col, row, col-1));
+        }
+         if (row < size-1){
+//bottom
+            neighbours.add(swap(row,col, row+1, col));
+        }
+        if (col < size-1){
+//left
+            neighbours.add(swap(row,col, row, col+1));
+
+        }
+
+
+        return neighbours;
+    }
+
+    private Board swap(int row, int col, int newRow, int newCol) {
+
+        int[][] newBoard = copyBoard(tiles);
+
+        newBoard[row][col] = tiles[newRow][newCol];
+        newBoard[newRow][newCol] = tiles[row][col];
+
+        return new Board(newBoard);
+    }
+
+    private int[][] copyBoard(int[][] original) {
+
+        int[][] newBoard = new int[size][size];
+
+        for (int i = 0; i < size; i++){
+            for(int j = 0; j < size; j++){
+
+                newBoard[i][j] = original[i][j];
+            }
+        }
+
+        return newBoard;
     }
 
     // a board that is obtained by exchanging any pair of tiles
     public Board twin() {
-        return null;
+
+        int swapRow = 0;
+        int swapCol = 0;
+
+        int twinRow = size -1;
+        int twinCol = size -1;
+
+
+        return swap(swapRow,swapCol,twinRow,twinCol);
     }
+
 
 
 }
